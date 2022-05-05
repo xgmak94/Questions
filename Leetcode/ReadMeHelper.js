190daf44f4f369ee5bee7e4749216a0e40710bfc
xgmak94 <xgmak94@gmail.com>
2022-05-04 22:10:42 -0700

    Fixed case sensitive links

--- Leetcode/ReadMeHelper.js
+++ Leetcode/ReadMeHelper.js
@@ -1,15 +1,18 @@
 //Helper function for the README file
 
 function READMELINK(qNum) {
+    var qNum = "2124. Check if All A's Appears Before All B's";
+    var link = "https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/";
+
     var arr = qNum.split(" ");
     var num = arr[0].slice(0, arr[0].length - 1);
     var qName = arr.slice(1);
 
     var readme = `${num} | [${qName.join(" ")}]`;
-    readme += `(https://leetcode.com/problems/${qName.join("-")}/)`;
+    readme += `${link}/)`;
     readme += ` | [Java](./Leetcode/${num}.%20${qName.join("%20")}/Solution.java)`;
 
     console.log(readme);
 }
 
-READMELINK("2260. Minimum Consecutive Cards to Pick Up");
+READMELINK();
