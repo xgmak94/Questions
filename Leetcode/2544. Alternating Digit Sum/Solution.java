e487ef8623c6008d9b39351cdc3cd95089655c7f
xgmak94 <xgmak94@gmail.com>
2023-02-02 22:24:56 -0800

    Added more questions

---
+++ Leetcode/2544. Alternating Digit Sum/Solution.java
@@ -0,0 +1,40 @@
+/* https://leetcode.com/problems/alternating-digit-sum/
+
+You are given a positive integer n. Each digit of n has a sign according to the following rules:
+
+The most significant digit is assigned a positive sign.
+Each other digit has an opposite sign to its adjacent digits.
+Return the sum of all digits with their corresponding sign.
+
+Example 1:
+Input: n = 521
+Output: 4
+Explanation: (+5) + (-2) + (+1) = 4.
+
+Example 2:
+Input: n = 111
+Output: 1
+Explanation: (+1) + (-1) + (+1) = 1.
+
+Example 3:
+Input: n = 886996
+Output: 0
+Explanation: (+8) + (-8) + (+6) + (-9) + (+9) + (-6) = 0.
+
+Constraints:
+1 <= n <= 109
+*/
+
+class Solution {
+    public int alternateDigitSum(int n) {
+        int sign = 1;
+        int sum = 0;
+        while(n != 0) {
+            sign *= -1;
+            sum += sign * (n % 10);
+            
+            n /= 10;
+        }
+        return sign * sum;
+    }
+}
